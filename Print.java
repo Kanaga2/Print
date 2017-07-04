e9081e7c438873a5150b62bc7e4a087acda0e333
Kanaga2 <priyakanaga112@gmail.com>
2017-07-04 21:56:36 +0530

    Create Print.java

---
+++ Print.java
@@ -0,0 +1,15 @@
+# Print
+import java.io.*;
+import java.util.*;
+public class Print
+{
+public static void main(String args[])
+{
+print(0);
+}
+private static void main(int i)
+{
+System.out.println((i%100)+1);
+print(i+i);
+}
+}
